cc89f61334f0a9fcd3d05e9d65c488f54280195c
KOS <ngongocthong1211@gmail.com>
2025-04-22 16:11:32 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
-import 'screens/intro_screen.dart'; 
-import 'screens/login_screen.dart'; 
-import 'screens/home_screen.dart'; 
+import 'screens/intro_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/forgotpassword_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('auth_token');
+
+  runApp(MyApp(initialRoute: token != null ? '/home' : '/'));
 }
 
 class MyApp extends StatelessWidget {
+  final String initialRoute;
+
+  const MyApp({Key? key, required this.initialRoute}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Admin App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: IntroScreen(),  
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: initialRoute,
       routes: {
-        '/login': (context) => LoginScreen(), 
-        '/home': (context) => HomeScreen(), 
+        '/': (context) => IntroScreen(),
+        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+        '/forgotpassword': (context) => ForgotPasswordScreen(),
+        '/register': (context) => RegisterScreen(),
       },
     );
   }
